7ead518c3d4af2bf5ffa9fe10fbf2f7c313686d2
JERSON-SAM <butawanjerson@gmail.com>
2024-06-25 00:58:57 -0700

    Update sbc-d6-act2.py

--- sbc-d6-act2.py
+++ sbc-d6-act2.py
@@ -1,17 +1,19 @@
+# Take input from the user for the number of rows and columns
 row = int(input("Enter the number of rows: "))
 col = int(input("Enter the number of columns: "))
-
+# Initialize a variable to track the current row (stars)
 stars = 0
-
+# Outer loop to iterate over each row
 while stars < row:
-    moon = 0
+    moon = 0 # Initialize a variable to track the current column (moon)
     while moon < col:
+        # Inner loop to iterate over each column within the current row
         if stars == 0 or stars == row -1 or moon == 0 or moon == col -1:
-            print("*", end="")
+            print("*", end="")# Print '*' if the current position is on the border
         else:
-            print(" ", end="")
-        moon += 1
-    print()
-    stars += 1 
+            print(" ", end="")# Print ' ' (space) if the current position is inside the border
+        moon += 1  # Increment the column counter
+    print() # Move to the next line after printing all columns for the current row
+    stars += 1 # Increment the row counter
    
        
